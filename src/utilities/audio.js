3dcea25a23a36c9cb22004fddad7c857cfb7aef5
Phil Mass <philrmass@gmail.com>
2022-11-28 23:43:33 -0800

    add basic components

--- src/utilities/audio.js
+++ src/utilities/audio.js
@@ -1,7 +1,7 @@
 export async function getMicrophone() {
   if (navigator.mediaDevices?.getUserMedia) {
     try {
-      const stream = await navigator.mediaDevices?.getUserMedia({ audio: true });
+      const stream = await navigator.mediaDevices?.getUserMedia({ audio: true, video: false });
       return stream;
     } catch (err) {
       console.error(`getUserMedia error: [${err}]`);
